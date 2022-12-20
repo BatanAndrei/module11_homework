e67f99a7ae731f65fc7fbcf81d6c368a610c2052
Batan Andrei <yabatan777@gmail.com>
2022-12-20 17:22:07 +0300

    initial commit

---
+++ task2.js
@@ -0,0 +1,22 @@
+let x = 10;
+
+function nNumber(n){
+  
+  if(n >= 2 && n < 1000){
+    
+for(var i = 2; i < n; i++){
+  
+    if(n % i === 0){
+      
+      return 'Не простое число';
+    }
+  return 'Простое число';
+    }
+  }
+   else if(n > 1000 || n < 2){
+     
+    return 'Данные не верны';
+  }
+}
+  
+console.log(nNumber(x));
